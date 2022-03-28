7427917c5b4cd2cd472624fa4b13949930a7d8f5
evgenkolesman <glavstroi_e@mail.ru>
2022-03-28 18:21:49 +0300

    Adding mongoDB to Albums service, need to fix: some configurations doesn`t configures from spring configuration

--- src/main/java/ru/koleson/photodiscoveryapp/PhotoDiscoveryAppApplication.java
+++ src/main/java/ru/koleson/photodiscoveryapp/PhotoDiscoveryAppApplication.java
@@ -3,6 +3,7 @@ package ru.koleson.photodiscoveryapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableEurekaServer
 @SpringBootApplication
